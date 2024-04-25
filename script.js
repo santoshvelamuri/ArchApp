15ddb3b1c0d713762fcf8f20d4af6f758cc2057b
santoshvelamuri <santosh.velamuri@gmail.com>
2024-04-25 23:13:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function to fetch JSON data from a file
 function fetchData() {
-    return fetch('https://github.com/santoshvelamuri/ArchApp/blob/main/data.json')
+    return fetch('data.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
